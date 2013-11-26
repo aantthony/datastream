6b841f34a9e58f83419bbbec183a15c0913aed77
Anthony Foster <git@af.id.au>
2013-11-26 22:38:54 +1100

    use a transform stream, assuming that the first buffer will never be exactly one character for now

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 datastream
 ==========
 
+Usage
+```js
+var encoder = new DataStream.Encoder();
+var output = fs.createWriteStream('/file.jsons.gz');
+decoder.pipe(zlip.createGzip()).pipe(output);
+
+encoder.write({x:3});
+encoder.write({x:4});
+encoder.end();
+
+encoder.on('finish', function () {
+  console.log('Done!');
+});
+```
+
+Reading:
+```js
+var decoder = new DataStream.Decoder();
+var input = fs.createReadStream('/file.jsons.gz').pipe(zip.createGunzip());
+input.pipe(decoder);
+decoder
+.on('data', function (err, data) {
+  console.log('value:', data.x);
+})
+.on('end', function () {
+  console.log('---- finished.');
+});
+```
 
 [![Build Status](https://travis-ci.org/aantthony/datastream.png?branch=master)](https://travis-ci.org/aantthony/datastream) [![Coverage Status](https://coveralls.io/repos/aantthony/datastream/badge.png?branch=master)](https://coveralls.io/r/aantthony/datastream?branch=master)
--- test/read/index.js
+++ test/read/index.js
@@ -1,24 +1,76 @@
 var fs = require('fs');
+var should = require('should');
+var zlip = require('zlib');
 var DataStream = require('../..');
 
 describe('DataStream', function () {
-  describe('#pipe', function () {
-    it('can be persisted to a file', function (done) {
-      var c = new DataStream(fs.createWriteStream('/tmp/test12341234.jsons'));
-      c.write({x:3});
-      c.write({x:3});
-      c.write({x:3});
-      c.write({x:3});
-      c.write({x:3});
-      c.end();
-      setTimeout(done, 300);
-    });
-    it('can read from a file', function (done) {
-      var c = new DataStream(fs.createReadStream('/tmp/test12341234.jsons'));
-      c.on('data', function (d) {
-        console.log('json', d);
+  ([false, true]).forEach(function (w) {
+    describe((w ? 'with' : 'without') + ' gzip', function () {
+      var filename = '/tmp/test12341234.pjson.gz';
+
+      it('can be persisted to a file', function (done) {
+        var c = new DataStream.Encoder();
+
+        if (w) {
+          c
+          .pipe(zlip.createGzip())
+          .pipe(fs.createWriteStream(filename))
+          .on('finish', done);
+        } else {
+          c
+          .pipe(fs.createWriteStream(filename))
+          .on('finish', done);
+        }
+
+        c.write({x:3});
+        c.write({x:3});
+        c.write({x:3});
+        c.write({x:3});
+        var longStr = '';
+        for(var i = 0; i < 4096; i++) {
+          longStr += '1';
+        }
+        c.write({x:3, str: longStr});
+
+        c.end();
       });
-      c.on('end', done);
+
+      if (w) {
+        it('should produce a small file', function (done) {
+          fs.stat(filename, function (err, info) {
+            (info.size < 1000).should.be.true;
+            done(err);
+          });
+        });
+      } else {
+        it('should produce a large file', function (done) {
+          fs.stat(filename, function (err, info) {
+            (info.size > 4096).should.be.true;
+            done(err);
+          });
+        });
+      }
+
+      it('can read from a file', function (done) {
+        var c = new DataStream.Decoder();
+        if (w) {
+          fs.createReadStream(filename).pipe(zlip.createGunzip()).pipe(c);
+        } else {
+          fs.createReadStream(filename).pipe(c);
+        }
+        c.read();
+        var hit = false;
+        c
+        .on('data', function (d) {
+          d.should.have.property('x');
+          hit = true;
+        })
+        .on('end', function (err) {
+          hit.should.be.true;
+          done(err);
+        });
+      });
     });
   });
+  
 });
